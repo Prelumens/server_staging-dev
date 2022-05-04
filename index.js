84e5aeb3f1d3ceee981c86d3e54f880bf830a643
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-05-04 19:06:11 +0800

    Bodyparser modification

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
 // apply middlewares
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
